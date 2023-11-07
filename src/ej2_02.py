64835c8a3f318bf6d4a9f08f0e6166ffa436d930
irolram <rrivan@iesrafaelalberti.es>
2023-11-07 19:43:07 +0100

    eje22

--- src/ej2_02.py
+++ src/ej2_02.py
@@ -3,3 +3,5 @@ Escribir un programa que almacene la cadena de caracteres contraseña en una var
 pregunte al usuario por la contraseña e imprima por pantalla si la contraseña introducida 
 por el usuario coincide con la guardada en la variable sin tener en cuenta mayúsculas y minúsculas.
 """
+def tal():
+    print
