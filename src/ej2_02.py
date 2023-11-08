930e0937e2a5819370f181434d3e8e82abcfbd3b
irolram <rrivan@iesrafaelalberti.es>
2023-11-08 19:07:20 +0100

    2

--- src/ej2_02.py
+++ src/ej2_02.py
@@ -3,5 +3,16 @@ Escribir un programa que almacene la cadena de caracteres contraseña en una var
 pregunte al usuario por la contraseña e imprima por pantalla si la contraseña introducida 
 por el usuario coincide con la guardada en la variable sin tener en cuenta mayúsculas y minúsculas.
 """
-def tal():
-    print
+
+def verificar_contra(contra_guardada, contra_usuario):
+    contra_usuario = contra_usuario.lower()
+    contra_guardada = contra_guardada
+    if contra_guardada == contra_usuario:
+        print("Contraseña correcta")
+    else:
+        print("contraseña incorrecta")
+
+contra_guardada = "contraseña"
+contra_usuario = input("introduzca la contraseña: ")
+verificar_contra(contra_guardada, contra_usuario)
+
