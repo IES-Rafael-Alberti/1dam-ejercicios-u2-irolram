04fd2312ffa0ff07e2daa91185ec714d2edcfbe8
irolram <rrivan@iesrafaelalberti.es>
2023-11-08 19:32:58 +0100

    hasta el 4

--- src/ej2_02.py
+++ src/ej2_02.py
@@ -5,14 +5,22 @@ por el usuario coincide con la guardada en la variable sin tener en cuenta mayú
 """
 
 def verificar_contra(contra_guardada, contra_usuario):
+
+    #Convertir las palabras a minusculas
     contra_usuario = contra_usuario.lower()
-    contra_guardada = contra_guardada
+    contra_guardada = contra_guardada.lower()
+
+    #Comprobar si coinciden las contraseñas
     if contra_guardada == contra_usuario:
         print("Contraseña correcta")
     else:
         print("contraseña incorrecta")
-
+    #Guardamos la contraseña en una variable
 contra_guardada = "contraseña"
+
+#Pedimos que ingrese la contraseña
 contra_usuario = input("introduzca la contraseña: ")
+
+#Llama a la funcion para verificar contraseña
 verificar_contra(contra_guardada, contra_usuario)
 
---
+++ src/ej2_04.py
@@ -0,0 +1,15 @@
+"""
+Escribir un programa que pida al usuario un número entero y muestre por pantalla si es par o impar.
+"""
+def parimpar():
+
+
+#Creamos variable para pedir el numero
+    numero = int(input("Dame un numero: "))
+    #verificamos si es par o impar
+    if numero % 2 == 0:
+        print("El numero es par")
+    else:
+        print("El numero es impar")
+
+parimpar()
