aa5b686cff5c667ebf03a63ebcdc64983f9886bf
irolram <rrivan@iesrafaelalberti.es>
2023-11-11 17:07:39 +0100

    entendiento main

--- src/ej2_04.py
+++ src/ej2_04.py
@@ -13,3 +13,13 @@ def parimpar():
         print("El numero es impar")
 
 parimpar()
+
+def main():
+    numero = int(input("Dame un numero: "))
+    if numero % 2 == 0:
+        print("El numero es par")
+    else:
+        print("El numero es impar")
+
+if __name__ == "__main__":
+    main()
