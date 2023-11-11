835763e429fef8a4ead53a1463ffa713bd8e7ea6
irolram <rrivan@iesrafaelalberti.es>
2023-11-11 17:55:27 +0100

    ej5

--- src/ej2_05.py
+++ src/ej2_05.py
@@ -4,6 +4,62 @@ o superiores a 1000 € mensuales. Escribir un programa que pregunte al usuario 
 mensuales y muestre por pantalla si el usuario tiene que tributar o no.
 """
 
-from ej2_04 import main
+def tributar(edad, ingresosAlMes):
 
-main()
+    return edad >= 16 and ingresosAlMes >= 1000
+
+def main():
+    edad = int(input("Escribe tú edad: \n"))
+    ingresosAlMes = int(input("Escriba sus ingresos: \n"))
+
+    if tributar(edad, ingresosAlMes):
+        print("Usted puede tributar")
+    else:
+        print("Usted no puede tributar")
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+def mayorDedad():
+    mayorEdad = int(input("Escribe tú edad: \n"))
+    
+    if mayorEdad < 16:
+        print("Usted no puede tributar")
+    else:
+        print("Usted puede tributar")
+
+def ingresos():
+    dinero = int(input("Escriba sus ingresos: \n"))
+
+    if dinero < 1000:
+        print("Usted no puede tributar")
+    else:
+        print("Usted puede tributar")
+
+
+def main():
+
+
+
+"""
