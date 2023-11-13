635ef553a23c2fba52ea239c00a9785d7b3767c1
irolram <rrivan@iesrafaelalberti.es>
2023-11-13 21:17:57 +0100

    ej2_07

--- src/ej2_07.py
+++ src/ej2_07.py
@@ -4,15 +4,4 @@ Escribir un programa que pregunte al usuario su renta anual y muestre por pantal
 """
 
 
-nombre = input("Ingrese su nombre: ")
-nombre_minusculas = nombre.lower()
-
-print("Nombre en minúsculas:", nombre_minusculas)
-
-nombre1 = "Juan"
-nombre2 = input("Ingrese otro nombre: ")
-
-if nombre1.lower() == nombre2.lower():
-    print("Los nombres son iguales (ignorando mayúsculas/minúsculas).")
-else:
-    print("Los nombres son diferentes.")
+sdasdad
